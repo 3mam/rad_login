c1e24bb910698cf50da41e465662300fcfd6f424
Konrad Chmielecki <konrad.chmielecki@outlook.com>
2020-11-04 23:12:07 +0100

    remove not used library

--- src/App.js
+++ src/App.js
@@ -2,20 +2,10 @@ import './App.css';
 import Signup from './components/Signup';
 import Login from './components/Login';
 import GoogleLogin from 'react-google-login';
-import ClientOAuth2 from 'client-oauth2';
 
 const responseGoogle = (response) => {
   console.log(response);
 }
-
-const githubAuth = new ClientOAuth2({
-  clientId: '775740881908-ckb5ts986vj0oirjnt97pbvffkd38kbt.apps.googleusercontent.com',
-  clientSecret: '60buzOqZoLf_-iJ5OU9YBCD1',
-  accessTokenUri: 'https://github.com/login/oauth/access_token',
-  authorizationUri: 'https://github.com/login/oauth/authorize',
-  redirectUri: 'http://example.com/auth/github/callback',
-  scopes: ['notifications', 'gist']
-})
 
 function App() {
   return (
